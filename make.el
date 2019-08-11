63053039a8af5c428a793b0aeebac16271181df1
Allen Li <darkfeline@felesatra.moe>
2019-08-11 03:22:17 -0700

    Fix indent

--- make.el
+++ make.el
@@ -2,7 +2,7 @@
       (s (progn (find-file "do-evil.el")
                 (read (current-buffer))))
       (template (progn (find-file "mimic-template.el")
-                (buffer-string))))
+                       (buffer-string))))
   (with-temp-buffer
     (insert (format template s))
     (write-region (point-min) (point-max) "mimic.el")))
